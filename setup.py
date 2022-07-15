9c8c53d979db1d59f506caaac63b2c2242701e2f
Prajwal Vandana <72575914+PrajwalVandana@users.noreply.github.com>
2022-07-14 18:10:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from os import name as sys_name
 from setuptools import setup
 
-dependencies = ['click', 'playsound', 'pygame>=1.9.2']
+dependencies = ['click', 'pygame>=1.9.2']
 if sys_name != 'nt':
     dependencies.append('getch')
 

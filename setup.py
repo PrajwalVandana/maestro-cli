1f6cad2a4b07a8b9510e4d1d6afb337c80e51b1d
PrajwalVandana <72575914+PrajwalVandana@users.noreply.github.com>
2022-07-20 22:00:52 -0700

    fixed ANOTHER error in setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,11 @@ from setuptools import setup
 
 setup(
     name='maestro',
-    author="Prajwal Vandana",
-    summary="A simple command line tool to play songs (or any audio files, really).",
-    keyword="music, sound, audio, music-player, cli",
     version='1.1.2',
+    author="Prajwal Vandana",
+    url="https://github.com/PrajwalVandana/maestro-cli",
+    description="A simple command line tool to play songs (or any audio files, really).",
+    keywords="music, sound, audio, music-player, cli",
     py_modules=['maestro'],
     install_requires=['click', 'just_playback', 'getch;platform_system!="Windows"'],
     entry_points={

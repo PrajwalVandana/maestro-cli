fedbe438d45b2158bdaca9f321e0a3e25df906f0
PrajwalVandana <72575914+PrajwalVandana@users.noreply.github.com>
2022-07-20 21:53:50 -0700

    fixed error in setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     keyword="music, sound, audio, music-player, cli",
     version='1.1.2',
     py_modules=['maestro'],
-    install_requires=['click', 'just_playback', 'getch;platform_system=="Linux"'],
+    install_requires=['click', 'just_playback', 'getch;platform_system!="Windows"'],
     entry_points={
         'console_scripts': [
             'maestro = maestro:cli',
